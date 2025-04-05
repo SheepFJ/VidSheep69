7caaa8f3ef511cca450da06dbb80086ab4c1a618
SheepFJ <xiaoenshixiaoyang@gmail.com>
2025-04-05 12:57:40 +0800

    0.12

--- main/main.js
+++ main/main.js
@@ -9,7 +9,7 @@ GitHub：https://github.com/SheepFJ/QuantumultX
 ============ Quantumult X ============
 
 [rewrite_local]
-^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/ url script-response-body https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/main.js
+^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/ url script-response-body https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/mainlogic.js
 [mitm]
 hostname = api.sheep.com
 
