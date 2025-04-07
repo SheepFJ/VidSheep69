f4e9796ce8c7ecccc1b4afa22c75a935b887fa28
SheepFJ <xiaoenshixiaoyang@gmail.com>
2025-04-07 18:37:44 +0800

    0.30autoimg

--- main/main_shadowrocket.js
+++ main/main_shadowrocket.js
@@ -5,4 +5,4 @@
 hostname = %APPEND% api.sheep.com  
   
 [Script]  
-VidSheep = type=http-response,pattern=^https?:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/.*,requires-body=1,script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/mainlogic.js
+VidSheep = type=http-response,pattern=^https?:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/,requires-body=1,script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/mainlogic.js
