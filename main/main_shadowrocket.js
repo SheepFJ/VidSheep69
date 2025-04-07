4afa7e118c8e97cc3a887da37acd246cb4afc55b
SheepFJ <xiaoenshixiaoyang@gmail.com>
2025-04-07 13:50:25 +0800

    0.29shad

--- main/main_shadowrocket.js
+++ main/main_shadowrocket.js
@@ -1,15 +1,8 @@
-#!name=VidSheep
-#!desc=用于PKC插件的视频与文本接口
-#!author=@Sheepfj
-#!homepage=https://github.com/SheepFJ/QuantumultX
-#!icon=https://raw.githubusercontent.com/SheepFJ/VidSheep69/main/icon/sheep.png
-#!date=2025-04-01
-
-[URL Rewrite]
-^ https: \/\/api\.sheep\.com\/sheep\/videoPolymerization\/ - reject
-
-[Script]
-VidSheep = type = http - response, pattern =^ https: \/\/api\.sheep\.com\/sheep\/videoPolymerization\/,script-path=https:/ / raw.githubusercontent.com / SheepFJ / VidSheep69 / refs / heads / main / main / mainlogic.js, requires - body=false, timeout = 10
-
-[MITM]
-hostname = % APPEND % api.sheep.com
+#!url=
+#!name=VidSheep  
+#!desc=VidSheep   
+[MITM]  
+hostname =%APPEND% api.sheep.com  
+  
+[Script]  
+VidSheep=type=http-response,pattern=https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/,requires-body=1,script-path= https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/mainlogic.js
